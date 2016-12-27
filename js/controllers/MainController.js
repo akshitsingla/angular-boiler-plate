c778fd58970e195700bbebcbe4f7221913d8a9dc
unknown <asingla@BLRL31980.iis.amadeus.net>
2016-12-27 16:29:47 +0530

    ng-repeat, ng-src, ng-click, etc.

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -7,4 +7,9 @@ app.controller('MainController', [
 		    price: 19,
     		pubdate: new Date('2014', '03', '08')
 		};
+	  $scope.fruits = ["Apple", "Banana", "Orange", "Pineapple"];
+	  $scope.imageURL = "http://www.opensourcephotography.org/themes/professional_theme/images/slide-image-1.jpg";
+	  $scope.clickFn = function(i) {
+	  	alert("This is fruit at index : " + i + ".");
+	  }
 	}]);
