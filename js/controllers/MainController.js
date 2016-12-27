10fec0ee4581565f89464b91024feb6c9176c0af
unknown <asingla@BLRL31980.iis.amadeus.net>
2016-12-27 16:03:03 +0530

    in-built filters

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -2,4 +2,9 @@ app.controller('MainController', [
 	'$scope', 
 	function($scope) { 
 	  $scope.title = 'Hello World!'; 
+	  $scope.product = { 
+		    name: 'The Book of Trees', 
+		    price: 19,
+    		pubdate: new Date('2014', '03', '08')
+		};
 	}]);
